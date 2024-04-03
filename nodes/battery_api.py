96e502991de22b0a6cd8c17b977a2865d8b9cbc2
Maaz Siddiqi <m.maaz_6@hotmail.com>
2024-04-03 03:04:10 -0400

    hid keys

--- nodes/battery_api.py
+++ nodes/battery_api.py
@@ -9,14 +9,14 @@ from iot_api_client.configuration import Configuration
 from std_msgs.msg import Int32 
 
 def get_token():
-    oauth_client = BackendApplicationClient(client_id="VGGQhtrkoqcvIfgb38Mxft0IQLVfZkgt")
+    oauth_client = BackendApplicationClient(client_id="client_id")
     token_url = "https://api2.arduino.cc/iot/v1/clients/token"
 
     oauth = OAuth2Session(client=oauth_client)
     token = oauth.fetch_token(
         token_url=token_url,
-        client_id="VGGQhtrkoqcvIfgb38Mxft0IQLVfZkgt",
-        client_secret="jtXDzUiRuGfy0ibx3gN1S5AKptwKLNHD6rKCTZfLjxDE6TiE2mUifij1AO5gT4va",
+        client_id="client_id",
+        client_secret="Client_secret",
         include_client_id=True,
         audience="https://api2.arduino.cc/iot",
     )
@@ -29,7 +29,7 @@ def interact_with_api(access_token):
     client_config = Configuration(host="https://api2.arduino.cc/iot")
     client_config.access_token = access_token
     client = iot.ApiClient(client_config)
-    thing_id = "a1a9e2bc-4df5-4c81-9a50-0b51bb4860eb"
+    thing_id = "thing_id"
 
     api = iot.PropertiesV2Api(client)
 
